92a9542872571622453d9a2c78e63ae34da1f102
Luiz-Brito <65183435+Luiz-Brito@users.noreply.github.com>
2022-05-22 15:25:22 -0300

    rotas

--- CRUDtw/frontend/src/app/app.module.ts
+++ CRUDtw/frontend/src/app/app.module.ts
@@ -1,18 +1,28 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+import { BodyComponent } from './components/template/body/body.component';
+import { HomeComponent } from './views/home/home.component';
+import { PedidoComponent } from './views/pedido/pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    HomeComponent,
+    PedidoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
